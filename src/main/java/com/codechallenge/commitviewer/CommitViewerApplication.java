51dea88d2a83aab0c6bbd4695f8facaf4fdef9a7
pbp <pbp333@gmail.com>
2021-07-13 18:46:36 +0100

    bootstrapped spring project

---
+++ src/main/java/com/codechallenge/commitviewer/CommitViewerApplication.java
@@ -0,0 +1,13 @@
+package com.codechallenge.commitviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codechallenge/commitviewer/CommitViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.commitviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
