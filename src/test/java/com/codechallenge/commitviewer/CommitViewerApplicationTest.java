2b88c75461625096cfac75d323807cc7f9266102
DESKTOP-A8FPN8A\pbp33 <pbp333@gmail.com>
2022-03-30 17:21:01 +0100

    renames a class

--- src/test/java/com/codechallenge/commitviewer/CommitViewerApplicationTest.java
+++ src/test/java/com/codechallenge/commitviewer/CommitViewerApplicationTest.java
@@ -3,11 +3,15 @@ package com.codechallenge.commitviewer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CommitViewerApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
+class CommitViewerApplicationTest {
+
+	// Application loads test
 	@Test
 	void contextLoads() {
+		assertThat(true).isTrue();
 	}
 
 }
