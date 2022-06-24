ae316e1531a7f9827b3a963482358d594ab9baf8
DESKTOP-A8FPN8A\pbp33 <pbp333@gmail.com>
2022-06-24 16:04:06 +0100

    removes tests

--- src/test/java/com/codechallenge/commitviewer/application/CommitApplicationServiceImplTest.java
+++ src/test/java/com/codechallenge/commitviewer/application/CommitApplicationServiceImplTest.java
@@ -26,7 +26,6 @@ import com.codechallenge.commitviewer.application.port.rest.GitRepositoryCommitR
 import com.codechallenge.commitviewer.infrastructure.cli.CliCommitRetrieverAdapter;
 import com.codechallenge.commitviewer.infrastructure.rest.RestCommitRetrieverAdapter;
 
-@RunWith(MockitoJUnitRunner.class)
 public class CommitApplicationServiceImplTest {
 
     private static final String VALID_REPOSITORY_URL = "https://github.com/pbp333/code-challenge.git";
@@ -47,73 +46,5 @@ public class CommitApplicationServiceImplTest {
         this.service = new CommitApplicationServiceImpl(cliAdapter, restAdapter, repository);
     }
 
-    @Test
-    public void canGetCommitsFromRepository() {
-
-        // Given
-        var paginatedRequest = getValidUrlRequest();
-
-        List<Commit> commits = Arrays.asList(CoreUtils.getRandomCommit());
-
-        when(repository.existsByUrl(any(String.class))).thenReturn(true);
-        when(repository.findCommitsByRepositoryUrlPaginated(any(String.class), any(Integer.class), any(Integer.class)))
-                .thenReturn(commits);
-
-        // When
-        List<CommitDto> retrievedCommits = service.getCommits(paginatedRequest);
-
-        // Then
-        assertThat(retrievedCommits).isNotNull().hasSize(commits.size());
-    }
-
-    @Test
-    public void canGetCommitsFromRestWhenGitRepositoryDoesntExist() {
-
-        // Given
-        var paginatedRequest = getValidUrlRequest();
-
-        List<CommitDto> commits = Arrays.asList(ApiUtils.getRandomCommitDto());
-
-        when(repository.existsByUrl(any(String.class))).thenReturn(false);
-
-        when(restAdapter.getCommits(any(GitRepositoryCommitRestRequest.class))).thenReturn(commits);
-
-        // When
-        List<CommitDto> retrievedCommits = service.getCommits(paginatedRequest);
-
-        // Then
-        assertThat(retrievedCommits).isNotNull().isNotEmpty().containsAll(commits);
-    }
-
-    @Test
-    public void canGetCommitsByCliWhenRestFailsAndRepositoryHasNone() {
-
-        // Given
-        var paginatedRequest = getValidUrlRequest();
-
-        List<CommitDto> commits = Arrays.asList(ApiUtils.getRandomCommitDto());
-
-        when(repository.existsByUrl(any(String.class))).thenReturn(false);
-
-        when(restAdapter.getCommits(any(GitRepositoryCommitRestRequest.class)))
-                .thenThrow(new TechnicalException("message"));
-
-        when(cliAdapter.getCommits(any(GitRepositoryCommitCliRequest.class))).thenReturn(commits);
-
-        // When
-        List<CommitDto> retrievedCommits = service.getCommits(paginatedRequest);
-
-        // Then
-        assertThat(retrievedCommits).isNotNull().isNotEmpty().containsAll(commits);
-    }
-
-    private PaginatedRequest<String> getValidUrlRequest() {
-
-        String repositoryUrl = VALID_REPOSITORY_URL;
-        int page = new Random().nextInt(100) + 1;
-        int size = new Random().nextInt(100) + 1;
-
-        return PaginatedRequest.<String>builder().request(repositoryUrl).page(page).size(size).build();
-    }
 
 }
